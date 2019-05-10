d6217b4913092aae167f2fe90989cbd61f2ddc23
e71655 <e71655@wnco.com>
2019-05-10 15:53:46 -0500

    Initial commit of spring boot example

---
+++ src/main/java/org/stanfordguillory/jenkinsxspringbootex2/DemoApplication.java
@@ -0,0 +1,13 @@
+package org.stanfordguillory.jenkinsxspringbootex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
